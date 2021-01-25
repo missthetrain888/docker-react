00fc62586e72354e4835dcba819a5cfe4d64e960
Gary G Liu <Gary.G.Liu@sunlife.com>
2021-01-25 16:04:57 -0500

    d

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.set('visits', 0);
 app.get('/', (req, res) => {
     process.exit(0);
     client.get('visits', (err, visits) => {
-      res.send('Number of visits is ' + visits);
+      res.send('Numbers of visits is ' + visits);
 	client.set('visits', parseInt(visits) + 1 );    
     });
 
